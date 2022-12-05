4316f7fd4ee1e021b67c14b7e00b41f0bb3df874
lssport <111240117+lssport@users.noreply.github.com>
2022-12-06 00:27:57 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ class App extends React.Component {
       <span className='title-md'>sisa uang kamu tersisa 75% lagi</span>
       </div>
       </div>
-
       <div className='row'>
       <div className='col-6'>
       <div className='card-wrapper p-4'>
@@ -38,8 +37,29 @@ class App extends React.Component {
 <span  className='title-sm'>pengeluaran</span>
 <h3 className='fw-bold'>Rp.500.000,-</h3><span className='title-sm text-ungu fw-bold'>50</span>
 <span className='title-sm'>transaksi</span>
-</div></div></div>
-</div></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div className='row mt-5'>
+  <div className='col-12 d-flex justify-content-between align-items-center'>
+    <h4>Ringkasan Transaksi</h4>
+    <div className='wtapper-button'>
+      <button className='button btn-ungu px-3 py-2 me-2'>Pemasukan<i class="bi bi-plus-circle-fill"></i></button>
+      <button className='button btn-pink px-3 py-2'>Pengeluaran<i class="bi bi-dash-circle-fill"></i></button>
+      </div>
+      </div>
+      <div className='row mt-4'>
+        <div className='col-12 d-flex justify-content-between align-items-center'>
+      <div className='d-flex align-items-text'>
+        <div className='icon-wrapper'>
+          <i class="bi bi-wallet2"></i>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>  
 </>
   );
 }
