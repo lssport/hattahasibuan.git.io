3e9cb4fb636bfbc5e3e544033af1cf5e4f32bb2b
lssport <111240117+lssport@users.noreply.github.com>
2022-12-12 19:03:56 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -75,7 +75,6 @@ this.state = {
   <div className='col-12 d-flex justify-content-between align-items-center'>
     <h4>Ringkasan Transaksi</h4>
     <div className='wrapper-button'>
-      <ModalCreate />
      <button className='button btn-ungu px-3 py-2 me-2'>Pemasukan<i className="bi bi-plus-circle-fill"></i></button>
      <button className='button btn-pink px-3 py-2'>Pengeluaran<i className="bi bi-dash-circle-fill"></i></button>
       </div>
