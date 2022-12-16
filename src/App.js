2f39b94da8f9b8a0fe808adc191bbe6559852219
Lssport <111240117+lssport@users.noreply.github.com>
2022-12-17 05:54:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
-import React, { useState } from 'react';
+import "./Modal.js";
+import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 class App extends React.Component {
@@ -76,7 +77,6 @@ this.state = {
   <div className='col-12 d-flex justify-content-between align-items-center'>
     <h4>Ringkasan Transaksi</h4>
     <div className='wrapper-button'>
-      <ModalCreate />
      <button className='button btn-ungu px-3 py-2 me-2'>Pemasukan<i className="bi bi-plus-circle-fill"></i></button>
      <button className='button btn-pink px-3 py-2'>Pengeluaran<i className="bi bi-dash-circle-fill"></i></button>
       </div>
@@ -105,52 +105,6 @@ this.state = {
   )
 }
 }
-class ModalCreate extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      show : false
-    }
-    this.handleClose = this.handleClose.bind(this);
-    this.handleShow = this.handleShow.bind(this);
-  }
-  handleClose () {
-    this.setstate ({
-      show : false
-    }) 
-  }
 
-  handleShow () {
-    this.setstate ({
-      show : true
-    }) 
-  }
-
-    render () {
-      return (
-    <>    
-      <Button variant="primary" onClick={this.handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={this.state.show} onHide={this.handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={this.handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-      
-  )
-}
-}
 
 export default App;
