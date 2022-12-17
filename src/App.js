15d7eee120d024b3ee97eff7f2623c828c0d0692
lssport <111240117+lssport@users.noreply.github.com>
2022-12-17 16:59:11 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -104,49 +104,4 @@ this.state = {
 }
 }
 
-class ModalCreate extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      show : false
-    }
-  }
-  handleClose () {
-    this.setstate ({
-      show : false
-    }) 
-  }
-
-  handleShow () {
-    this.setstate ({
-      show : true
-    }) 
-  }
-
-    render () {
-      return (
-   
-<>
-      <Button variant="primary" onClick={this.handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={this.state.show} onHide={this.handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={this.handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  )
-}
-}
 export default App;
