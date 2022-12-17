4ebd7142e4d5155947f33659e8bced4790db651c
lssport <111240117+lssport@users.noreply.github.com>
2022-12-17 16:47:19 +0700

    1 file modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import react from "react";
+import App from "./App";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
