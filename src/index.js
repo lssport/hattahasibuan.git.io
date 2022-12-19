ab5c9f78b0ca6fdbd851d22a6ea9dab5ce87aa38
lssport <111240117+lssport@users.noreply.github.com>
2022-12-19 12:05:07 +0700

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./App";
 import "./style.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
